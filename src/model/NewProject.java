048e1f18cfaa0332d906d5208f6e7aa7812844be
SourabhTripathi21 <Tripathi.S@northeastern.edu>
2021-09-26 20:20:49 -0400

    Assignment 1

---
+++ src/model/NewProject.java
@@ -0,0 +1,192 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ *
+ * @author saura
+ */
+
+public class NewProject {
+    
+    
+private String name;
+private String address;
+private String DateOfBirth;
+private String phonenum;
+private String faxnum;
+private String email;
+private String socialsecnum;
+private int medicalrecnum;
+private int healthnum;
+private int bankaccnum;
+private String certificatenum;
+private String Vehiclenum;
+private int serialnum;
+private String linkedin;
+private String ipaddress;
+private int uniqueidentificationnumber;
+private String photos;
+private String biometric;
+private String path="";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public void setDateOfBirth(String DateOfBirth) {
+        this.DateOfBirth = DateOfBirth;
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+
+    public String getFaxnum() {
+        return faxnum;
+    }
+
+    public void setFaxnum(String faxnum) {
+        this.faxnum = faxnum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSocialsecnum() {
+        return socialsecnum;
+    }
+
+    public void setSocialsecnum(String socialsecnum) {
+        this.socialsecnum = socialsecnum;
+    }
+
+    public int getMedicalrecnum() {
+        return medicalrecnum;
+    }
+
+    public void setMedicalrecnum(int medicalrecnum) {
+        this.medicalrecnum = medicalrecnum;
+    }
+
+    public int getHealthnum() {
+        return healthnum;
+    }
+
+    public void setHealthnum(int healthnum) {
+        this.healthnum = healthnum;
+    }
+
+    public int getBankaccnum() {
+        return bankaccnum;
+    }
+
+    public void setBankaccnum(int bankaccnum) {
+        this.bankaccnum = bankaccnum;
+    }
+
+    public String getCertificatenum() {
+        return certificatenum;
+    }
+
+    public void setCertificatenum(String certificatenum) {
+        this.certificatenum = certificatenum;
+    }
+
+    public String getVehiclenum() {
+        return Vehiclenum;
+    }
+
+    public void setVehiclenum(String Vehiclenum) {
+        this.Vehiclenum = Vehiclenum;
+    }
+
+    public int getSerialnum() {
+        return serialnum;
+    }
+
+    public void setSerialnum(int serialnum) {
+        this.serialnum = serialnum;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIpaddress() {
+        return ipaddress;
+    }
+
+    public void setIpaddress(String ipaddress) {
+        this.ipaddress = ipaddress;
+    }
+
+    public int getUniqueidentificationnumber() {
+        return uniqueidentificationnumber;
+    }
+
+    public void setUniqueidentificationnumber(int uniqueidentificationnumber) {
+        this.uniqueidentificationnumber = uniqueidentificationnumber;
+    }
+
+    public String getPhoto() {
+        return photos;
+    }
+
+    public void setPhoto(String image) {
+        this.photos = image;
+    }
+
+    public String getBiometric() {
+        return biometric;
+    }
+
+    public void setBiometric(String biometric) {
+        this.biometric = biometric;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+    
+}
+
